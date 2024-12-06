a091a6eb4985c0b679305718108e2fe2eb2a7551
Rudendru Rahul <120588284+Rudendru-03@users.noreply.github.com>
2024-12-06 17:33:59 +0530

    react redux practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,11 @@
 import React, { useContext } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  increment,
+  decrement,
+  incrementByAmount,
+  reset,
+} from "./store/slice/counterSlice";
 // import Counter from "./component/Counter";
 // import { CounterContext } from "./constext/counterContext";
 import { CartContext } from "./context/cartContext";
@@ -6,17 +13,29 @@ import Card from "./component/card";
 import Cart from "./component/cart";
 
 const App = () => {
+  const dispatch = useDispatch();
+  const count = useSelector((state) => state.counter);
+  console.log("count :", count.value);
+
   // const counterState = useContext(CounterContext);
   // console.log("counterState", counterState);
 
-  const cartState = useContext(CartContext);
-  console.log(cartState);
+  // const cartState = useContext(CartContext);
+  // console.log(cartState);
   return (
     <>
-      <h2>E commerce Minutes </h2>
+      {/* <h2>E commerce Minutes </h2>
       <Card />
 
-      <Cart />
+      <Cart /> */}
+
+      <h1>counter : {count.value}</h1>
+      <button onClick={() => dispatch(increment())}>Increment</button>
+      <button onClick={() => dispatch(decrement())}>Decrement</button>
+      <button onClick={() => dispatch(incrementByAmount(10))}>
+        IncrementByAmount
+      </button>
+      <button onClick={() => dispatch(reset())}>Reset</button>
     </>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,17 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import { CartProvider } from "./context/cartContext";
+import { store } from "./store/store";
+import { Provider } from "react-redux";
 
 createRoot(document.getElementById("root")).render(
   <>
     <StrictMode>
-      <CartProvider>
+      {/* <CartProvider> */}
+      <Provider store={store}>
         <App />
-      </CartProvider>
+      </Provider>
+      {/* </CartProvider> */}
     </StrictMode>
   </>
 );
